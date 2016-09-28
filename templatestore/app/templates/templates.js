997f87121740267db25a916e3de006b41963f0e2
Aleksa <aleksa47@gmail.com>
2016-09-28 12:34:45 +0200

    Finished tutorial for template store

--- templatestore/app/templates/templates.js
+++ templatestore/app/templates/templates.js
@@ -12,10 +12,26 @@ angular.module('templateStore.templates',['ngRoute'])
     });
 }])
 
-.controller('TemplatesCtrl', ['$scope',function($scope){
-  console.log($scope);
+.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('json/templates.json').success(function (data) {
+    $scope.templates = data;
+  });
 }])
 
-.controller('TemplateDetailsCtrl', ['$scope',function($scope){
-  console.log($scope);
+.controller('TemplateDetailsCtrl', ['$scope',
+                                    '$http',
+                                    '$routeParams',
+                                    '$filter',
+                                    function($scope, $http, $routeParams, $filter){
+  var templateId = $routeParams.templateId;
+  $http.get('json/templates.json').success(function (data) {
+    $scope.template = $filter('filter')(data, function (d) {
+      return d.id == templateId;
+    })[0];
+    $scope.mainImage = $scope.template.images[0].name;
+
+    $scope.setImage = function (image) {
+      $scope.mainImage = image.name;
+    }
+  });
 }]);
